0a134cbba008741ad8e925e3df0537834146c30e
Eugene <60469435+geugenm@users.noreply.github.com>
2023-09-10 22:34:29 +0300

    Release problems fix

--- app/src/main/java/github/alexzhirkevich/studentbsuby/util/NotificationCreator.kt
+++ app/src/main/java/github/alexzhirkevich/studentbsuby/util/NotificationCreator.kt
@@ -42,7 +42,7 @@ class NotificationCreator(
         PendingIntent.getActivity(
             context, 0, Intent(context, MainActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            }, 0
+            }, PendingIntent.FLAG_IMMUTABLE
                                  )
     }
 
