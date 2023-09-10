9a4e0e21ce653ba658e2374f1da568250375a427
Eugene <glebajk@gmail.com>
2023-09-10 17:22:13 +0300

    Removed firebase. Fixed compilation error

--- app/src/main/java/github/alexzhirkevich/studentbsuby/repo/RemoteConfigRepository.kt
+++ app/src/main/java/github/alexzhirkevich/studentbsuby/repo/RemoteConfigRepository.kt
@@ -2,31 +2,22 @@ package github.alexzhirkevich.studentbsuby.repo
 
 import android.content.SharedPreferences
 import com.google.android.gms.tasks.Task
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import github.alexzhirkevich.studentbsuby.BuildConfig
 import github.alexzhirkevich.studentbsuby.util.sharedPreferences
 import kotlinx.coroutines.suspendCancellableCoroutine
-import org.json.JSONObject
 import javax.inject.Inject
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
-private const val G_PLAY = "google_play"
-private const val TELEGRAM = "telegram"
-private const val MAIL = "mail"
 private const val STABLE_VER = "stable_version"
 private const val LATEST_VER = "latest_version"
-private const val VER_CODE = "code"
-private const val VER_NAME = "name"
-private const val VER_DESC = "desc"
 
-private const val UPDATE_PROP_DELAY =  3 * 24 * 60 * 60 * 1000L
+private const val UPDATE_PROP_DELAY = 3 * 24 * 60 * 60 * 1000L
 
 data class ApplicationVersion(
-    val code : Int,
-    val name : String,
-    val desc : String
+    val code: Int,
+    val name: String,
+    val desc: String
 )
 
 class RemoteConfigRepository @Inject constructor(
@@ -36,50 +27,37 @@ class RemoteConfigRepository @Inject constructor(
     private var lastUpdateProp by sharedPreferences(preferences, 0L)
 
     init {
-        val config = FirebaseRemoteConfig.getInstance()
 
-        val settings = FirebaseRemoteConfigSettings.Builder()
-            .setMinimumFetchIntervalInSeconds(10)
-            .build()
-
-        config.setConfigSettingsAsync(settings)
     }
 
-    suspend fun update(){
-        FirebaseRemoteConfig.getInstance().fetchAndActivate().await()
+    fun update() {
+
     }
 
-    private suspend fun getVersion(name: String) : ApplicationVersion? = kotlin.runCatching {
-        val version = JSONObject(FirebaseRemoteConfig.getInstance().getString(name))
-        update()
-        return ApplicationVersion(
-            code = version.getInt(VER_CODE),
-            name = version.getString(VER_NAME),
-            desc = version.getString(VER_DESC)
-        )
+    private suspend fun getVersion(name: String): ApplicationVersion? = kotlin.runCatching {
+        null
     }.getOrNull()
 
-    suspend fun getLatestVersionIfNeeded() : ApplicationVersion? =
+    suspend fun getLatestVersionIfNeeded(): ApplicationVersion? =
         getVersion(LATEST_VER)?.takeIf {
             BuildConfig.VERSION_CODE < it.code &&
                     System.currentTimeMillis() - lastUpdateProp >= UPDATE_PROP_DELAY
         }.also { lastUpdateProp = System.currentTimeMillis() }
 
-    suspend fun getMinimumStableVersionIfNeeded() : ApplicationVersion? =
+    suspend fun getMinimumStableVersionIfNeeded(): ApplicationVersion? =
         getVersion(STABLE_VER)?.takeIf { BuildConfig.VERSION_CODE < it.code }
 
-    fun telegram() : String = kotlin.runCatching{
-       return  FirebaseRemoteConfig.getInstance()
-           .getString(TELEGRAM)
+    fun telegram(): String = kotlin.runCatching {
+        null
     }.getOrNull().orEmpty()
 
-    fun mail() : String = kotlin.runCatching {
-        FirebaseRemoteConfig.getInstance().getString(MAIL)
+    fun mail(): String = kotlin.runCatching {
+        null
     }.getOrNull().orEmpty()
 
 }
 
-suspend fun <T> Task<T>.await() : T = suspendCancellableCoroutine { cont ->
+suspend fun <T> Task<T>.await(): T = suspendCancellableCoroutine { cont ->
     addOnCanceledListener {
         cont.cancel()
     }.addOnSuccessListener {
--- app/src/main/java/github/alexzhirkevich/studentbsuby/repo/SettingsRepository.kt
+++ app/src/main/java/github/alexzhirkevich/studentbsuby/repo/SettingsRepository.kt
@@ -1,17 +1,12 @@
 package github.alexzhirkevich.studentbsuby.repo
 
-import android.content.Context
 import android.content.SharedPreferences
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.ExperimentalComposeUiApi
 import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.crashlytics.FirebaseCrashlytics
-import dagger.hilt.android.qualifiers.ApplicationContext
 import github.alexzhirkevich.studentbsuby.util.sharedPreferences
-import github.alexzhirkevich.studentbsuby.workers.SyncWorkerManager
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 
@@ -24,20 +19,13 @@ import javax.inject.Inject
 @ExperimentalPagerApi
 class SettingsRepository @Inject constructor(
     preferences: SharedPreferences,
-    @ApplicationContext context: Context,
-    private val synchronizationWorkerManager: SyncWorkerManager,
 ) {
-    var synchronizationEnabled by sharedPreferences(preferences,true){
-//        with(synchronizationWorkerManager){
-//            if (it) run() else stop()
-//        }
+    var synchronizationEnabled by sharedPreferences(preferences, true) {
     }
 
-    var collectStatistics by sharedPreferences(preferences,true){
-        FirebaseAnalytics.getInstance(context).setAnalyticsCollectionEnabled(it)
+    var collectStatistics by sharedPreferences(preferences, true) {
     }
 
-    var collectCrashlytics by sharedPreferences(preferences,true){
-        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(it)
+    var collectCrashlytics by sharedPreferences(preferences, true) {
     }
 }
